b688a5feb35c8f01debe6ad84ff7565a809f7261
Dan Simpson <dan.simpson@gmail.com>
2026-04-28 16:15:42 -0700

    nit: black and white logo, rev package, add sim test

--- site/docs/components/logo/nordstern.tsx
+++ site/docs/components/logo/nordstern.tsx
@@ -1,5 +1,11 @@
 export function NordsternLogo() {
   return (
-    <img src="/nordstern.png" alt="Nordstern" className="h-[26px] w-auto" height={26} width={132} />
+    <img
+      src="/nordstern.png"
+      alt="Nordstern"
+      className="h-[26px] w-auto grayscale"
+      height={26}
+      width={132}
+    />
   );
 }
